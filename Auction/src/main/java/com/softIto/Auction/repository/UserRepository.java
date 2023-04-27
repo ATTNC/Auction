448367b51e4bcd586a8d342d75402d54195353d7
derslik3-24 <derslik3-24@sinif3-24-iMac.local>
2023-04-27 13:53:57 +0300

    services and repositories created

---
+++ Auction/src/main/java/com/softIto/Auction/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.softIto.Auction.repository;
+
+import com.softIto.Auction.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
